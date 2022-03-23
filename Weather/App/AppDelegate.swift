6b76a57edad3d54379cef98076e090f8ba8861ed
Anatolii-sx <s8.8@bk.ru>
2022-03-23 21:14:32 +0300

    #1 Initial commit

---
+++ Weather/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by Анатолий Миронов on 23.03.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
